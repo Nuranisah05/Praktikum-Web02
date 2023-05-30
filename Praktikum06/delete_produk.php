b29c7d5d88902715afb1265eeb53b011ae6abea1
Nuranisah05 <114374016+Nuranisah05@users.noreply.github.com>
2023-05-30 22:21:29 +0700

    Delete Praktikum06 directory

--- Praktikum06/delete_produk.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'dbkoneksi.php';
-$delete = $_GET['iddel'];
-$sql = "DELETE FROM produk WHERE id=?";
-$st = $dbh->prepare($sql);
-$st->execute([$delete]);
-
-header('location:list_produk.php');
-?>
